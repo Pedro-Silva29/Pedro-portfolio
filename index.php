141e8c0b2acfee8ec15b7f961a0f72fad2822b56
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-IH8VEDN>
2021-01-11 12:11:40 -0300

    pedro-portfolio-original

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+        <!--Icomoon-->
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css.css" />
+        <!--css-->
+        <link rel="stylesheet" href="assets/css/" />
+        <!--js-->
+        <link rel="stylesheet" href="assets/js/" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card-group">
+                        <div class="card">
+                            <img src="assets/img/fancy.JPG" class="card-img-top img-fluid" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary"><a href="paginas/p1.php">PLUGIN-1</a></button>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="assets/img/fancy.JPG" class="card-img-top img-fluid" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div>
+                                <a href="plugins/p2/"><button class="btn btn-primary">PLUGIN-1</button></a> 
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <img src="assets/img/fancy.JPG" class="card-img-top img-fluid" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div>
+                                <button class="btn btn-primary"><a href="paginas/p1.php">PLUGIN-1</a></button>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    </body>
+                    </html>
